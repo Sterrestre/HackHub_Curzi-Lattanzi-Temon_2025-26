128ab16d0dc6dee36237867ae6c497ca1058953c
matilde-curzi <matilde.curzi@studenti.unicam.it>
2026-02-13 19:50:25 +0100

    Aggiunta del codice delle classi concrete Utente, RoleFactory, Organizzatore, Mentore e Giudice, della classe astratta RuoloPartecipazione e dell'enumerazione RuoliStaff

---
+++ Codice/src/main/java/it/unicam/cs/ids/model/RoleFactory.java
@@ -0,0 +1,22 @@
+package it.unicam.cs.ids.model;
+
+public class RoleFactory {
+
+    public Organizzatore assegnaOrganizzatore(Utente utente, Hackathon hackathon) {
+        Organizzatore o = new Organizzatore(utente, hackathon);
+        utente.addRuolo(o);
+        return o;
+    }
+
+    public Giudice assegnaGiudice(Utente utente, Hackathon hackathon) {
+        Giudice g = new Giudice(utente, hackathon);
+        utente.addRuolo(g);
+        return g;
+    }
+
+    public Mentore assegnaMentore(Utente utente, Hackathon hackathon) {
+        Mentore m = new Mentore(utente, hackathon);
+        utente.addRuolo(m);
+        return m;
+    }
+}
