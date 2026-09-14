cbc1ca545956f62b9fe005db48371213d2bdc897
Ester Temon <stella.temon@gmail.com>
2026-09-14 23:20:26 +0200

    Forzato il profilo dev per il test HackControllerTest

--- src/test/java/it.unicam.cs.ids/controller/HackControllerTest.java
+++ src/test/java/it.unicam.cs.ids/controller/HackControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -43,6 +44,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
  */
 @WebMvcTest(HackController.class)
 @Import(SecurityConfig.class)
+@ActiveProfiles("dev")
 class HackControllerTest {
 
     @Autowired
