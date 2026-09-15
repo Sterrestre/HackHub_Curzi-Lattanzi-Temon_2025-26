a377be1d792e0e4fb060b9b5db5c889a0a0a841b
Ester Temon <stella.temon@gmail.com>
2026-09-15 08:47:40 +0200

    Cambiamento della classe MockMailSender (conflitto tra annotazione Service e Bean)

--- src/main/java/it/unicam/cs/ids/service/infrastructure/gmail/MockMailSender.java
+++ src/main/java/it/unicam/cs/ids/service/infrastructure/gmail/MockMailSender.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 // Pattern: Adapter (Mock)
-@Service
+
 @ConditionalOnProperty(name = "gmail.enabled", havingValue = "false", matchIfMissing = true)
 public class MockMailSender implements MailSender {
 
